558b42e7b7764ce52ae9b9d45d3c6d1d4e56ef55
Brett Johnson <brettthomasjohnson@gmail.com>
2023-10-10 15:16:35 -0700

    update description file formatting

--- generate_description.R
+++ generate_description.R
@@ -3,7 +3,14 @@ get_packages <- function(file_path) {
   lines <- readLines(file_path)
   packages <- unique(grep("library\\(", lines, value = TRUE))
   packages <- gsub("library\\(([^)]+)\\)", "\\1", packages)
+  packages <- gsub("\"|\\s+", "", packages) # Remove quotes and extra spaces
   return(packages)
+}
+
+# Get installed version of a package
+get_package_version <- function(package) {
+  version <- as.character(packageVersion(package))
+  return(paste0("(", ">= ", version, ")"))
 }
 
 # List all Rmd files in your project directory
@@ -12,15 +19,26 @@ rmd_files <- list.files(pattern = "*.Rmd")
 # Extract package names from all Rmd files
 all_packages <- unique(unlist(lapply(rmd_files, get_packages)))
 
-# Create DESCRIPTION file
-description_content <- paste0(
-  "Package: MyProject\n",
-  "Type: Project\n",
-  "License: What license it uses\n",
-  "Imports:\n",
-  paste(all_packages, collapse = ",\n"),
-  "\n"
-)
+# Generate the formatted package strings
+formatted_packages <- sapply(all_packages, function(pkg) {
+  if (requireNamespace(pkg, quietly = TRUE)) {
+    version_info <- get_package_version(pkg)
+    return(paste0("    ", pkg, " ", version_info, ","))
+  } else {
+    return(NULL) # Skip if the package is not installed
+  }
+}, USE.NAMES = FALSE)
 
-# Write the DESCRIPTION file
-writeLines(description_content, "DESCRIPTION")
+# Concatenate all the package strings into a single string
+formatted_package_string <- paste(formatted_packages, collapse = "\n")
+
+# Remove the trailing comma from the last package
+formatted_package_string <- sub(",$", "", formatted_package_string)
+
+# Remove the old DESCRIPTION file if it exists
+if (file.exists("DESCRIPTION")) {
+  file.remove("DESCRIPTION")
+}
+
+# Use the formatted package string in the `usethis::use_description` function
+usethis::use_description(fields = list(Title = "About page", Imports = formatted_package_string))
