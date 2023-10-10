9bc4335e8c8d5678abd3863ecbe1077bde624472
Brett Johnson <brettthomasjohnson@gmail.com>
2023-10-10 15:35:54 -0700

    add manual entry

--- generate_description.R
+++ generate_description.R
@@ -30,7 +30,11 @@ formatted_packages <- sapply(all_packages, function(pkg) {
 }, USE.NAMES = FALSE)
 
 # Concatenate all the package strings into a single string
-formatted_package_string <- paste(formatted_packages, "usethis (>= 2.1.6)", collapse = "\n")
+formatted_package_string <- paste(formatted_packages, collapse = "\n")
+
+manual_entry <- "    usethis (>= 2.1.6),"
+# Prepend the manual entry to the existing formatted package string
+formatted_package_string <- paste(manual_entry, formatted_package_string, sep = "\n")
 
 # Remove the trailing comma from the last package
 formatted_package_string <- sub(",$", "", formatted_package_string)
